08c98babcae537ed7f3d098d6537d01811cccd0f
牛严 <632176758@qq.com>
2016-08-05 14:19:05 +0800

    Initial Commit

---
+++ NYRefresh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NYRefresh
+//
+//  Created by 牛严 on 16/8/5.
+//  Copyright © 2016年 牛严. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NYRefresh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NYRefresh
+//
+//  Created by 牛严 on 16/8/5.
+//  Copyright © 2016年 牛严. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
